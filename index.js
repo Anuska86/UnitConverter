e20eeb67eddebe4862614437816b22172f8a3f62
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-10-15 11:35:07 +0200

    units

--- index.js
+++ index.js
@@ -32,6 +32,42 @@ function convertUnits() {
     const toPounds = round(value * 2.204);
     const toKilos = round(value / 2.204);
     result = `${value} kilos = ${toPounds} pounds | ${value} pounds = ${toKilos} kilos`;
+  } else if (unit === "celsius") {
+    emoji = "🌡️";
+
+    const toFahrenheit = round((value * 9) / 5 + 32);
+    const toCelsius = round(((value - 32) * 5) / 9);
+    result = `${value}°C = ${toFahrenheit}°F | ${value}°F = ${toCelsius}°C`;
+  } else if (unit === "kmh") {
+    emoji = "🚗";
+
+    const toMph = round(value * 0.621371);
+    const toKmh = round(value / 0.621371);
+    result = `${value} km/h = ${toMph} mph | ${value} mph = ${toKmh} km/h`;
+  } else if (unit === "joule") {
+    emoji = "🔥";
+
+    const toCalories = round(value * 0.239006);
+    const toJoules = round(value / 0.239006);
+    result = `${value} joules = ${toCalories} calories | ${value} calories = ${toJoules} joules`;
+  } else if (unit === "megabyte") {
+    emoji = "💾";
+
+    const toGigabytes = round(value / 1024);
+    const toMegabytes = round(value * 1024);
+    result = `${value} MB = ${toGigabytes} GB | ${value} GB = ${toMegabytes} MB`;
+  } else if (unit === "hour") {
+    emoji = "🕒";
+
+    const toMinutes = round(value * 60);
+    const toHours = round(value / 60);
+    result = `${value} hours = ${toMinutes} minutes | ${value} minutes = ${toHours} hours`;
+  } else if (unit === "sqm") {
+    emoji = "🧭";
+
+    const toSqft = round(value * 10.7639);
+    const toSqm = round(value / 10.7639);
+    result = `${value} m² = ${toSqft} ft² | ${value} ft² = ${toSqm} m²`;
   }
 
   conversionEl.textContent = `${emoji} ${result}`;
